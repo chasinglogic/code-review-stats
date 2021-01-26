8b02f6acbe5fce78d13589678fb5b6138c931fd9
Mathew Robinson <mathew@chasinglogic.io>
2021-01-26 13:13:09 +0000

    wip

---
+++ download_all_data.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import subprocess
+
+BACKEND_REPOSITORIES = [
+    "MPBX",
+    "TransactionService",
+    "MediaService",
+    "SearchService",
+]
+
+FRONTEND_REPOSITORIES = [
+    "Flamingo",
+    "Toucan",
+]
+
+DATA_DIR = "data"
+BACKEND_DATA_DIR = os.path.join(DATA_DIR, "backend")
+FRONTEND_DATA_DIR = os.path.join(DATA_DIR, "frontend")
+
+if not os.path.isdir("data"):
+    os.makedirs("data")
+
+if not os.path.isdir(BACKEND_DATA_DIR):
+    os.makedirs(BACKEND_DATA_DIR)
+
+if not os.path.isdir(FRONTEND_DATA_DIR):
+    os.makedirs(FRONTEND_DATA_DIR)
+
+
+for repository in FRONTEND_REPOSITORIES:
+    output_file = os.path.join(FRONTEND_DATA_DIR, f"{repository}.json")
+    print("Loading PR data for", repository, "to", output_file)
+    subprocess.run(
+        [
+            sys.executable,
+            "./download_data.py",
+            "-o",
+            output_file,
+            "mpb-com",
+            repository,
+        ]
+    )
+
+
+for repository in BACKEND_REPOSITORIES:
+    output_file = os.path.join(BACKEND_DATA_DIR, f"{repository}.json")
+    print("Loading PR data for", repository, "to", output_file)
+    subprocess.run(
+        [
+            sys.executable,
+            "./download_data.py",
+            "-o",
+            output_file,
+            "mpb-com",
+            repository,
+        ]
+    )
--- download_data.py
+++ download_data.py
@@ -5,33 +5,51 @@ import sys
 
 import requests
 
-parser = argparse.ArgumentParser(description="Downloads PR review data from GitHub for a given repo")
+parser = argparse.ArgumentParser(
+    description="Downloads PR review data from GitHub for a given repo"
+)
 parser.add_argument(
-    "repo_owner", help="the owner of a GitHub repo. For 'https://github.com/Microsoft/TypeScript' would be 'Microsoft'"
+    "repo_owner",
+    help="the owner of a GitHub repo. For 'https://github.com/Microsoft/TypeScript' would be 'Microsoft'",
 )
 parser.add_argument(
     "repo_name",
-    help=
-    "the name of a GitHub repo of the above owner. For 'https://github.com/Microsoft/TypeScript' would be 'TypeScript'"
+    help="the name of a GitHub repo of the above owner. For 'https://github.com/Microsoft/TypeScript' would be 'TypeScript'",
 )
-parser.add_argument("-n", "--num-prs", type=int, help="the total number of PRs to download from the repo", default=1000)
-parser.add_argument("--prs-per-batch", type=int, help="the number of PRs to download per request", default=100)
-parser.add_argument("-o", "--output-file", help="file to output; if omitted uses stdout")
+parser.add_argument(
+    "-n",
+    "--num-prs",
+    type=int,
+    help="the total number of PRs to download from the repo",
+    default=1000,
+)
+parser.add_argument(
+    "--prs-per-batch",
+    type=int,
+    help="the number of PRs to download per request",
+    default=100,
+)
+parser.add_argument(
+    "-o", "--output-file", help="file to output; if omitted uses stdout"
+)
 args = parser.parse_args()
 
-API_TOKEN_KEY = 'GH_API_TOKEN'
+API_TOKEN_KEY = "GITHUB_TOKEN"
 if not API_TOKEN_KEY in os.environ:
-    print(f"There must be a '{API_TOKEN_KEY}' environment variable defined", file=sys.stderr)
+    print(
+        f"There must be a '{API_TOKEN_KEY}' environment variable defined",
+        file=sys.stderr,
+    )
     exit(1)
 
 token = os.environ[API_TOKEN_KEY]
-ENDPOINT = 'https://api.github.com/graphql'
+ENDPOINT = "https://api.github.com/graphql"
 HEADERS = {
     "Authorization": f"Bearer {token}",
     "Accept": "application/vnd.github.starfire-preview+json",
 }
 
-query = '''
+query = """
 query($repoOwner: String!, $repoName: String!, $prBefore: String, $prCount: Int = 100){
   repository(owner: $repoOwner, name: $repoName) {
     pullRequests(last: $prCount, before: $prBefore) {
@@ -88,7 +106,7 @@ fragment ReviewerInfo on RequestedReviewer {
     name
   }
 }
-'''
+"""
 
 start_cursor = None
 has_previous_page = True
@@ -107,18 +125,18 @@ while has_previous_page and len(all_nodes) < args.num_prs:
     response.raise_for_status()
     result = response.json()
 
-    if 'errors' in result:
-        print(result['errors'], file=sys.stderr)
+    if "errors" in result:
+        print(result["errors"], file=sys.stderr)
         exit(1)
 
-    pull_requests = result['data']['repository']['pullRequests']
-    start_cursor = pull_requests['pageInfo']['startCursor']
-    has_previous_page = pull_requests['pageInfo']['hasPreviousPage']
+    pull_requests = result["data"]["repository"]["pullRequests"]
+    start_cursor = pull_requests["pageInfo"]["startCursor"]
+    has_previous_page = pull_requests["pageInfo"]["hasPreviousPage"]
 
-    all_nodes.extend(pull_requests['nodes'])
+    all_nodes.extend(pull_requests["nodes"])
     print(f"Loaded {len(all_nodes)} pull requests", file=sys.stderr)
 else:
     print("Loaded all pull requests successfully", file=sys.stderr)
 
-output_file = open(args.output_file, 'w') if args.output_file else sys.stdout
+output_file = open(args.output_file, "w") if args.output_file else sys.stdout
 output_file.write(json.dumps(all_nodes, indent=2) + "\n")
